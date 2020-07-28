0bf6fdec48c8620e9f34ab234102cdc5da50a796
Denis Pyshev <git@gemelen.net>
2020-07-28 23:04:45 +0300

    Ugly fix for ugly code

--- project/SparkBuild.scala
+++ project/SparkBuild.scala
@@ -299,7 +299,7 @@ object SparkBuild extends PomBuild {
       }
 
       var failed = 0
-      analysis.infos.allInfos.foreach { case (k, i) =>
+      analysis.asInstanceOf[sbt.internal.inc.Analysis].infos.allInfos.foreach { case (k, i) =>
         i.reportedProblems foreach { p =>
           val deprecation = p.message.contains("is deprecated")
 
